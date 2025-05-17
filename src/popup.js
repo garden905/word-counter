f00ce21f623f482d1a6475fc25e6f3e357621cc4
garden905 <balboa.0905@gmail.com>
2025-05-17 15:44:58 +0900

    文字数指定にトグルスイッチのUIを追加

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,8 @@
 document.getElementById("save").onclick = () => {
   const figure_number = document.getElementById("figure_number").value;
+  const toggleFigure = document.getElementById("toggle_figure").value;
   const position = document.getElementById("position").value;
-  chrome.storage.local.set({ figure_number,position }, () => {
+  chrome.storage.local.set({ figure_number,toggle_figure,position }, () => {
     alert("保存しました");
   });
 };
@@ -9,6 +10,17 @@ document.getElementById("save").onclick = () => {
 document.addEventListener("DOMContentLoaded", () => {
   chrome.storage.local.get(["figure_number","position"], (result) => {
     document.getElementById("figure_number").value = result.figure_number || 0;
+    document.getElementById("toggle_figure").value = result.toggle_figure || false;
     document.getElementById("position").value = result.position || "top";
+
+        // チェックボックスの状態に応じて入力欄を有効/無効にする
+        const toggleFigure = document.getElementById("toggle_figure");
+        const figureNumber = document.getElementById("figure_number");
+        figureNumber.disabled = !toggleFigure.checked;
+    
+        toggleFigure.addEventListener("change", () => {
+          figureNumber.disabled = !toggleFigure.checked;
+        });
   });
+   
 });
